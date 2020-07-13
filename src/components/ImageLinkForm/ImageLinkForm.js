d8dbe4b5e876558f805cffbfb65d95af5aa6bf1b
milanjacimovic <jacimovicmilanfon@gmail.com>
2020-07-13 17:02:07 +0200

    Changed home page text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
-        {"This Magic Brain will detect faces in your pictures. Give it a try."}
+        {
+          "This Magic Brain will detect faces in your pictures. Just paste image url down below and enjoy."
+        }
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
